57729e05e67c46aeb57720d97bf993e736bba289
dseif <david.c.seifried@gmail.com>
2013-01-04 16:44:16 -0500

    Added some videojs specific unit tests

--- wrappers/videojs/popcorn.HTMLVideojsVideoElement.unit.js
+++ wrappers/videojs/popcorn.HTMLVideojsVideoElement.unit.js
@@ -10,6 +10,46 @@ var players = {},
     var wrapper = Popcorn.HTMLVideojsVideoElement( id );
     players[QUnit.config.current.testName] = wrapper;
     return wrapper;
+  },
+  playerSpecificAsyncTests: function() {
+    var video;
+    asyncTest( "Videojs 01 - Videojs wrapper can be created with Videojs object", 1, function() {
+      var elem = document.createElement( "video" ),
+          parent = document.getElementById( "video" );
+
+      elem.src = testData.videoSrc;
+      parent.appendChild( elem );
+
+      _V_( elem ).ready(function() {
+        video = testData.createMedia( this );
+
+        video.addEventListener( "play", function playListener() {
+          video.removeEventListener( "play", playListener, false );
+          ok( true, "Videojs wrapper created using Videojs object" );
+          parent.removeChild( parent.children[ 0 ] );
+          video.pause();
+          start();
+        }, false );
+        video.play();
+      });
+    });
+
+    asyncTest( "Videojs 02 - destroying a Videojs wrapper created with a Videojs object does not remove the video", 1, function() {
+      var elem = document.createElement( "video" ),
+          parent = document.getElementById( "video" );
+
+      elem.src = testData.videoSrc;
+      parent.appendChild( elem );
+
+      _V_( elem ).ready(function() {
+        video = testData.createMedia( this );
+
+        console.log( video );
+        video._util.destroy();
+        equal( parent.children[ 0 ].children[ 0 ], elem, "Video element still exists after being destroyed" );
+        start();
+      });
+    });
   }
 };
 
